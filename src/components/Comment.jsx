8b8647cdb3e46c329760473737418d8f71e24b1f
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-04 10:43:53 -0700

    add back link on Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,7 +6,7 @@ function Comment() {
 
 
    useEffect(()=>{
-       fetch("https://fine-pink-deer.cyclic.app/afficheCommentaires")
+       fetch("https://ebath1.vercel.app/afficheCommentaires")
        .then(res =>res.json())
        .then(data =>setContactData(data))
        .catch(err => console.log(err));
