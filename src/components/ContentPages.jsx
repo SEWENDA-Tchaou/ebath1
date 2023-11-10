d2b836413f347bd76342b3baa4754e52649688cf
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-09 17:22:15 -0800

    add img_admin h-full

--- src/components/ContentPages.jsx
+++ src/components/ContentPages.jsx
@@ -1,7 +1,7 @@
 
 function ContentPages() {
   return (
-    <div className="text-3xl flex justify-center items-center  w-full bg-cover bg-center bg-no-repeat h-[80vh] bg-[url('/src/assets/equipe.jpg')]">
+    <div className="text-3xl flex justify-center items-center  w-full bg-cover bg-center bg-no-repeat bg-[url('/src/assets/equipe.jpg')]">
       <div className='bg-cafe/50 text-center text-blanc py-[2%] shadow-noir-2xl w-full h-full'>
         <p className="uppercase">
           BIENVENUE EBATH ADMIN
