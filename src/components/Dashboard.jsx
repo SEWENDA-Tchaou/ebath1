01b16952304b84bd258e4f18e171c0edb5e9eadb
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-14 09:49:50 -0800

    authentificate

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import HeaderDashboard from '../PageDashboard/HeaderDashboard'
 
 function Dashboard() {
   // const [auth, setAuth] = useState(false)
+  const auth = false
   axios.defaults.withCredentials = true;
   useEffect(()=>{
     axios.get("https://ebath-back1.vercel.app")
@@ -24,7 +25,7 @@ function Dashboard() {
 
   return (
     <div className='bg-noir/10'>
-    {/* {auth ? */}
+    {auth ?
       <React.Fragment>
         <div>
           <HeaderDashboard/>
@@ -38,9 +39,9 @@ function Dashboard() {
           </section>
         </div>
       </React.Fragment>
-     {/* :
+      :
       <div>no data</div>
-      }  */}
+      }  
     </div>
   )
 }
