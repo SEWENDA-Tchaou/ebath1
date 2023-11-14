5c942119a17d0c8256d05bccd3eb96119993b2a4
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-14 11:46:06 -0800

    navigate again

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,12 +3,13 @@ import React, { useEffect } from 'react';
 import Sidebar from './Sidebar';
 import ContentPages from './ContentPages';
 import HeaderDashboard from '../PageDashboard/HeaderDashboard'
-
+import { useParams } from 'react-router-dom';
 function Dashboard() {
+  const { dashboardId } = useParams();
   // const [auth, setAuth] = useState(false)
   axios.defaults.withCredentials = true;
   useEffect(()=>{
-    axios.get("https://ebath-back1.vercel.app")
+    axios.get(`https://ebath-back1.vercel.app/dashboard/${dashboardId}`)
     .then((response)=>{
       // console.log(auth)
       // if(
@@ -20,7 +21,7 @@ function Dashboard() {
         // setAuth(false);
       // }
     })
-  }, [])
+  }, [dashboardId])
 
   return (
     <div className='bg-noir/10'>
